5a6bf6c5eb27e5dad2a90d231150d39de8d3318a
luisaguilerac <luis.aguilerac25@gmail.com>
2016-01-25 14:09:25 -0300

    Controladores principales de la plataforma de reserva

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,15 +2,12 @@
 <html>
     <head>
         <title>Sistema de reversa de horas</title>
-
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-
         <style>
             html, body {
                 height: 100%;
             }
-
             body {
                 margin: 0;
                 padding: 0;
@@ -19,18 +16,15 @@
                 font-weight: 100;
                 font-family: 'Lato';
             }
-
             .container {
                 text-align: center;
                 display: table-cell;
                 vertical-align: middle;
             }
-
             .content {
                 text-align: center;
                 display: inline-block;
             }
-
             .title {
                 font-size: 96px;
             }
